42f961d5796dee874ada260e299ce1081fa208a3
Ed <edmundluong@gmail.com>
2019-01-19 18:00:58 -0800

    finished app

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express(); // initialize an object
 const port = 3000; // default port to listen on 
 
 
-var task = ["buy socks","practice nodejs"];
+var task = ["write blog post","practice nodejs"];
 var completed = ["finish react"];
 
 //application settings
@@ -14,10 +14,10 @@ app.set("views","./views")
 app.set("view engine","pug")
 
 
-
 //middleware
 app.use(bodyParser.urlencoded({extended: true})); //set up body parser
-
+//express wont allow access to this folder; need this line to have css work explicitly
+app.use(express.static("public"));
 
 //routes
 app.post("/addtask",(req,res) => {
@@ -32,15 +32,46 @@ app.post("/addtask",(req,res) => {
 });
 
 app.post("/removetask",(req,res)=>{
-    
+
+    //find all of the done tasks
+    var completedTask = req.body.check;
+
+    //check if there is only one option choosen
+    if(typeof completedTask === "string"){
+        
+        //add the one task to the completed task array
+        completed.push(completedTask);
+        
+        //get the index of the one checked list item
+        var index = task.indexOf(completedTask);
+
+        //find the index of the array of the element to remove; splice the element at the column
+        if (index > -1){
+            task.splice(index,1);
+        }
+    }
+    else{
+        //make sure it is an array of task completed
+        if(typeof completedTask === 'object'){
+            //look through all of the completed tasks and add them
+            for(var i= 0;i < completedTask.length; i++){
+                
+                //add each completed task in the array
+                completed.push(completedTask[i]);
+                
+                //remove the completed task from array of tasks
+                task.splice(task.indexOf(completedTask[i],1));
+                
+            }
+        }
+    }  
+    //reload the root route with the changes
+    res.redirect("/");
 });
 
 app.get("/",(req,res) =>{
     res.render("index",{task:task,completed:completed}); //this will have the application render what is in the index views folder
 });
-
-
-
 
 app.listen(port,() =>{
     console.log(`running on port ${port}`)});
